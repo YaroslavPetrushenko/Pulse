68898ffeb2ef67c88c215c17cb6a4e3357b98cb1
Yaroslav <petrushenkoia11@gmail.com>
2026-03-20 15:36:12 +0600

    7

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const wss = new WebSocket.Server({ server });
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "static")));
+app.use(express.static(__dirname));
 
 const DB_FILE = path.join(__dirname, "cloud.db");
 const db = new sqlite3.Database(DB_FILE);
