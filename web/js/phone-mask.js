233c0e8df191b329a6cf21c7152b88985c0b4640
Yaroslav <petrushenkoia11@gmail.com>
2026-03-22 13:24:09 +0600

    1

--- web/js/phone-mask.js
+++ web/js/phone-mask.js
@@ -1,6 +1,13 @@
-function applyPhoneMask(input, countryCode) {
-    input.oninput = () => {
+function applyPhoneMask(input) {
+    input.addEventListener("input", () => {
+        // Оставляем только цифры
         let digits = input.value.replace(/\D/g, "");
-        input.value = countryCode + " " + digits;
-    };
+
+        // Ограничиваем ровно 8 цифр
+        if (digits.length > 8) {
+            digits = digits.slice(0, 8);
+        }
+
+        input.value = digits;
+    });
 }
